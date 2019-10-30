30ea69652dbe89614076bc2d0719509b75f8ca2c
aang <devanshguptamrt@gmail.com>
2019-10-30 16:16:46 +0530

    added workorder states

--- src/Fixed.js
+++ src/Fixed.js
@@ -1,7 +1,7 @@
 //this file contains fixed 
 
 
-export let GroupTypes={
+export const GroupTypes={
     Account:1,
     User:2,
     Workplace:3,
@@ -9,14 +9,18 @@ export let GroupTypes={
     Operation:5,
     Route:6
 }
-export let Genders=[
+export const Genders=[
     {key:0,value:0,text:"Male"},
     {key:1,value:1,text:"Female"}
 ]
-export let IdProofs=[
+export const IdProofs=[
     {key:1,value:1,text:"Aadhar"},
     {key:2,value:2,text:"Voter Id"},
     {key:3,value:3,text:"Passport"},
     {key:4,value:4,text:"Driving License"}
    
 ]
+export const WorkOrderStates={
+    "Not Started":1,
+
+}
